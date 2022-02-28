c01d691dc65ebc076279c53d3dc67cea723a5cc8
digitsu <digitsu@gmail.com>
2022-02-28 16:42:36 +0900

    Set the cpu to be a time delay instead of a throw. Added a call cycles() which frobots can use to see how many cycles they have left. Along with rig_info which returns the cycle cost map they should be able to manage their CPU usage without incurring the penalty

--- apps/frobots/test/frobots_rigs_test.exs
+++ apps/frobots/test/frobots_rigs_test.exs
@@ -1,13 +1,12 @@
 defmodule FrobotsTest do
   use ExUnit.Case
-  doctest Frobots
   alias Fubars.Frobot
 
   test "create a rabbit" do
     assert {:ok, pid} = GenServer.start_link(Frobot, %Frobot{name: "rabbit1", brain_path: "src/rabbit.lua" } )
     assert Process.alive?(pid)
 
-    GenServer.call(pid, :init_tank)
+    Frobot.prestart(pid)
     assert state = Frobot.get_state(pid)
     vm = state.vm
     damage = vm |> Operate.VM.eval!("return function() return damage() end") |> Operate.VM.exec_function!([])
@@ -17,6 +16,12 @@ defmodule FrobotsTest do
     assert x > 0
     assert y > 0 # yeah maybe this test may fail due to the random nature of the loc, but meh
     rig_info = vm |> Operate.VM.eval!("return function() return rig_info() end") |> Operate.VM.exec_function!([])
-    assert rig_info == %{"model" => "Tank mk1"}
+    assert rig_info == %{"model" => "Tank mk1", "cpu_cycle_cost" => %{"cannon" => 25, "damage" => 1, "drive" => 40, "loc_x" => 1, "loc_y" => 1, "scan" => 2, "speed" => 1}}
+
+    cycles = vm |> Operate.VM.eval!("return function() return cycles() end") |> Operate.VM.exec_function!([])
+    assert cycles == 57
+    _res = vm |> Operate.VM.eval!("return function() return cannon( 90, 100 ) end") |> Operate.VM.exec_function!([])
+    cycles = vm |> Operate.VM.eval!("return function() return cycles() end") |> Operate.VM.exec_function!([])
+    assert cycles == 32
   end
 end
