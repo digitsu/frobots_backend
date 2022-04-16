152049de594ea04d5217a6b46fda0ce2005cb928
digitsu <digitsu@gmail.com>
2022-04-16 11:28:04 +0900

    FRO-73 Implement channel subscription for client added event encoding that actually works.

--- apps/frobots_web/lib/frobots_web/channels/arena_channel.ex
+++ apps/frobots_web/lib/frobots_web/channels/arena_channel.ex
@@ -47,55 +47,88 @@ defmodule FrobotsWeb.ArenaChannel do
     true
   end
 
-  def handle_info({:fsm_state, frobot, fsm_state}, socket) do
-    broadcast(socket, "arena_event", [:fsm_state, frobot, fsm_state])
+# -----------------------------------------------------------
+
+  @doc """
+
+  # This function marshals an internal elixir event of the form
+
+    {:event_name, arg1, arg2, arg3, {arg4, arg5} ...}
+
+  #into a map serializable over the wire of the form
+
+    %{event: :event_name, args: [ arg1, arg2, arg3, [arg4, arg5] ... ] }
+
+  #Which is received (after JSON encoding and decoding) as:
+
+    %{"event" => "event_name", "args" => [ arg1, arg2, arg3, [arg4, arg5] ... ] }
+
+  """
+  defp encode_event(evt_tuple) do
+    [evt | args] = Tuple.to_list(evt_tuple)
+    %{event: evt, args: args}
+  end
+
+  @impl true
+  def handle_info({:fsm_state, _frobot, _fsm_state} = msg, socket) do
+    broadcast(socket, "arena_event", encode_event(msg))
     {:noreply, socket}
   end
 
-  def handle_info({:scan, frobot, deg, res}, socket) do
-    broadcast(socket, "arena_event", [:scan, frobot, deg, res])
+  @impl true
+  def handle_info({:scan, _frobot, _deg, _res} = msg, socket) do
+    broadcast(socket, "arena_event", encode_event(msg))
     {:noreply, socket}
   end
 
-  def handle_info({:damage, frobot, damage}, socket) do
-    broadcast(socket, "arena_event", [:damage, frobot, damage])
+  @impl true
+  def handle_info({:damage, _frobot, _damage} = msg, socket) do
+    broadcast(socket, "arena_event", encode_event(msg))
     {:noreply, socket}
   end
 
-  def handle_info({:create_tank, frobot, loc}, socket) do
-    broadcast(socket, "arena_event", [:create_tank, frobot, loc])
+  @impl true
+  def handle_info({:create_tank, _frobot, _loc} = msg, socket) do
+    broadcast(socket, "arena_event", encode_event(msg))
     # nop because tanks are created by the init, and we can ignore this message
     # may use this if in future init does not place the tank at loc, and only gives it a name and id.
     {:noreply, socket}
   end
 
-  def handle_info({:move_tank, frobot, loc, heading, speed}, socket) do
-    broadcast(socket, "arena_event", [:move_tank, frobot, loc, heading, speed])
+  @impl true
+  def handle_info({:move_tank, frobot, loc, heading, speed} = msg, socket) do
+    inspect [:move_tank, frobot, loc, heading, speed]
+    broadcast(socket, "arena_event", encode_event(msg))
     {:noreply, socket}
   end
 
-  def handle_info({:kill_tank, frobot}, socket) do
-    broadcast(socket, "arena_event", [:kill_tank, frobot])
+  @impl true
+  def handle_info({:kill_tank, _frobot} = msg, socket) do
+    broadcast(socket, "arena_event", encode_event(msg))
     {:noreply, socket}
   end
 
-  def handle_info({:create_miss, m_name, loc}, socket) do
-    broadcast(socket, "arena_event", [:create_miss, m_name, loc])
+  @impl true
+  def handle_info({:create_miss, _m_name, _loc} = msg, socket) do
+    broadcast(socket, "arena_event", encode_event(msg))
     {:noreply, socket}
   end
 
-  def handle_info({:move_miss, m_name, loc}, socket) do
-    broadcast(socket, "arena_event", [:move_miss, m_name, loc])
+  @impl true
+  def handle_info({:move_miss, _m_name, _loc} = msg, socket) do
+    broadcast(socket, "arena_event", encode_event(msg))
     {:noreply, socket}
   end
 
-  def handle_info({:kill_miss, m_name}, socket) do
-    broadcast(socket, "arena_event", [:kill_miss, m_name])
+  @impl true
+  def handle_info({:kill_miss, _m_name} = msg, socket) do
+    broadcast(socket, "arena_event", encode_event(msg))
     {:noreply, socket}
   end
 
-  def handle_info({:game_over, name}, socket) do
-    broadcast(socket, "arena_event", [:game_over, name])
+  @impl true
+  def handle_info({:game_over, _name} = msg, socket) do
+    broadcast(socket, "arena_event", encode_event(msg))
     {:noreply, socket}
   end
 
