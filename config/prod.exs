4e5805f44aa9656f956ccdbaf541219e8b2719ee
digitsu <digitsu@gmail.com>
2022-07-28 18:02:08 +0900

    server host change to 'internal'

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :frobots_web, FrobotsWeb.Endpoint,
-  url: [host: "beta.frobots.io", port: 80],
+  url: [host: "internal.frobots.io", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
