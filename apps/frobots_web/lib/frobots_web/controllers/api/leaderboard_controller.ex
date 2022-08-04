15fadd7feaed75d886f5938097c598bf5a258dd7
digitsu <digitsu@gmail.com>
2022-08-04 16:42:19 +0900

    FRO-167 Fix issue when trying to kill the DynRigSuper

--- apps/frobots_web/lib/frobots_web/controllers/api/leaderboard_controller.ex
+++ apps/frobots_web/lib/frobots_web/controllers/api/leaderboard_controller.ex
@@ -4,22 +4,24 @@ defmodule FrobotsWeb.Api.LeaderboardController do
 
   action_fallback FrobotsWeb.FallbackController
 
-  @dummy_response   [
-      %{"attempts" => 213, "points" => 0, "username" => "god"},
-      %{"attempts" => 7, "points" => 0, "username" => "bob"},
-      %{"attempts" => 5, "points" => 0, "username" => "jerry"}
-    ]
+  @dummy_response [
+    %{"attempts" => 213, "points" => 0, "username" => "god"},
+    %{"attempts" => 7, "points" => 0, "username" => "bob"},
+    %{"attempts" => 5, "points" => 0, "username" => "jerry"}
+  ]
 
   def index(conn, _params) do
     # populate agent
     check_all = fn res ->
-      Enum.all?(res, & (:ok == &1))
+      Enum.all?(res, &(:ok == &1))
     end
-    case check_all.(Events.prep_leaderboard_entries()) do 
-      true -> entries = Events.send_leaderboard_entries()
-        conn |> put_status(200) |> json(entries)
-      false ->
 
+    case check_all.(Events.prep_leaderboard_entries()) do
+      true ->
+        entries = Events.send_leaderboard_entries()
+        conn |> put_status(200) |> json(entries)
+
+      false ->
         entries = Events.send_leaderboard_entries()
         conn |> put_status(200) |> json(entries)
     end
