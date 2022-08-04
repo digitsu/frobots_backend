7e7f007b462c898e21c1635545935d254e9578b5
digitsu <digitsu@gmail.com>
2022-08-04 19:53:29 +0900

    build script changes

--- contrib/build.sh
+++ contrib/build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 export DOCKER_HOST=unix:///tmp/docker.sock
+chmod 0600 $SSH_KEY
 ssh -i $SSH_KEY -o StrictHostKeyChecking=no -N -L '/tmp/docker.sock':'/var/run/docker.sock' -f deployer@172.105.215.192
 docker image build -t elixir/frobots_backend .
 
