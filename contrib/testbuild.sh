4ca317b249980f2a96684be58fb816a72a78360b
digitsu <digitsu@gmail.com>
2022-08-05 00:00:58 +0900

    build patch

---
+++ contrib/testbuild.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+docker stop frobots_backend
+docker run --rm -dp $PORT:$PORT -e POOL_SIZE -e PORT -e DATABASE_URL -e SECRET_KEY_BASE -e ADMIN_USER -e ADMIN_PASS --network frobots-network --name frobots_backend elixir/frobots_backend
+
