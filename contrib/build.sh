5d7b87b2ec95fefd7fc5e9642fb978c4f5624b71
digitsu <digitsu@gmail.com>
2022-08-12 06:07:02 +0000

    Update contrib/build.sh

--- contrib/build.sh
+++ contrib/build.sh
@@ -2,7 +2,15 @@
 
 export DOCKER_HOST=unix:///tmp/docker.sock
 chmod 0600 $SSH_KEY
-ssh -i $SSH_KEY -o StrictHostKeyChecking=no -N -L '/tmp/docker.sock':'/var/run/docker.sock' -f deployer@172.105.215.192
+
+if [[$CI_COMMIT_BRANCH == "main"]]
+then
+    ssh -i $SSH_KEY -o StrictHostKeyChecking=no -N -L '/tmp/docker.sock':'/var/run/docker.sock' -f deployer@172.105.215.192
+elif [[$CI_COMMIT_BRANCH == "dev"]]
+then
+    ssh -i $SSH_KEY -o StrictHostKeyChecking=no -N -L '/tmp/docker.sock':'/var/run/docker.sock' -f deployer@172.104.73.245
+fi
+
 docker image build -t elixir/frobots_backend -f ./Dockerfile .
 docker stop frobots_backend
 docker run --rm -dp $PORT:$PORT -e POOL_SIZE -e PORT -e DATABASE_URL -e SECRET_KEY_BASE -e ADMIN_USER -e ADMIN_PASS --network frobots-network --name frobots_backend elixir/frobots_backend
