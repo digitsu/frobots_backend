34e6467072385fa0c4082c0157efc1a9780c2974
digitsu <digitsu@gmail.com>
2022-08-30 01:39:40 +0000

    Update contrib/build.sh

--- contrib/build.sh
+++ contrib/build.sh
@@ -15,5 +15,6 @@ fi
 ssh -i /tmp/.ssh.key -o StrictHostKeyChecking=no -N -L '/tmp/docker.sock':'/var/run/docker.sock' -f deployer@$ip
 docker image build -t elixir/frobots_backend -f ./Dockerfile .
 docker stop frobots_backend
+docker container prune --force
 docker run --rm -dp $PORT:$PORT -e POOL_SIZE -e PORT -e DATABASE_URL -e SECRET_KEY_BASE -e ADMIN_USER -e ADMIN_PASS --network frobots-network --name frobots_backend elixir/frobots_backend
 docker exec frobots_backend bin/frobots_backend eval "FrobotsWeb.Release.migrate"
