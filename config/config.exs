7d411a544a6194c469df8df189e98856e3f8e7c5
Sumit Pugalia <sumit_pugalia@yahoo.com>
2022-10-31 09:54:59 +0530

    channels + receive all BE events

--- config/config.exs
+++ config/config.exs
@@ -114,6 +114,11 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+config :phoenix_client,
+  socket: [
+    url: "ws://localhost:4000/socket/websocket"
+  ]
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
