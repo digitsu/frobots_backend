2abba50edd3ea1ceee3334e2a9da94fff51ee104
digitsu <digitsu@gmail.com>
2022-11-17 06:12:41 +0000

    Update contrib/build_new.sh

--- contrib/build_new.sh
+++ contrib/build_new.sh
@@ -27,8 +27,14 @@ rm /tmp/.ssh.key || true
 docker image build --build-arg SENDGRID_API_KEY  -t elixir/frobots_backend -f ./Dockerfile .
 
 docker stop frobots_backend ||true
+docker stop postgres || true
 docker network create frobots-network ||true
+docker create volume postgres_home ||true
+
 docker container prune --force ||true
-docker run --rm -dp $PORT:$PORT -e SENDGRID_API_KEY -e POOL_SIZE -e PORT -e DATABASE_URL -e SECRET_KEY_BASE -e ADMIN_USER -e ADMIN_PASS --network frobots-network --name frobots_backend elixir/frobots_backend
+
+docker run --detach --rm --network frobots-network --network-alias postgres-server -e POSTGRES_PASSWORD=$POSTGRES_PASS -e POSTGRES_USER=$POSTGRES_USER -v postgres_home:/home/${POSTGRES_USER} --name postgres postgres:12-bullseye
+
+docker run --rm -dp $PORT:$PORT -e SENDGRID_API_KEY -e POOL_SIZE -e PORT -e DATABASE_URL_STAGING -e SECRET_KEY_BASE -e ADMIN_USER -e ADMIN_PASS --network frobots-network --name frobots_backend elixir/frobots_backend
 
 docker exec frobots_backend bin/frobots_backend eval "FrobotsWeb.Release.migrate"
