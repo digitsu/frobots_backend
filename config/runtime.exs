2db7e215321350d9c01ee7825e3428326a4e6860
digitsu <digitsu@gmail.com>
2022-11-17 06:26:06 +0000

    Update config/runtime.exs

--- config/runtime.exs
+++ config/runtime.exs
@@ -85,22 +85,12 @@ if config_env() == :prod do
   # See https://hexdocs.pm/swoosh/Swoosh.html#module-installation for details.
 
   database_url =
-    System.get_env("DATABASE_URL") ||
-      raise """
-      environment variable DATABASE_URL is missing.
-      For example: ecto://USER:PASS@HOST/DATABASE
-      """
+    System.get_env("DATABASE_URL") || nil
 
-  maybe_ipv6 = if System.get_env("ECTO_IPV6"), do: [:inet6], else: []
 
-  config :frobots, Frobots.Repo,
-    # ssl: true,
-    url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    socket_options: maybe_ipv6
-
+  if database_url == nil, do:
   database_url =
-    System.get_env("DATABASE_URL") ||
+    System.get_env("DATABASE_URL_STAGING") ||
       raise """
       environment variable DATABASE_URL is missing.
       For example: ecto://USER:PASS@HOST/DATABASE
