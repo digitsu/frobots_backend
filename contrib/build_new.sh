0f1cd6fa07eae7747c2ee66981ba5d4baa957b0a
digitsu <digitsu@gmail.com>
2023-03-13 07:27:34 +0000

    Update build_new.sh

--- contrib/build_new.sh
+++ contrib/build_new.sh
@@ -75,7 +75,7 @@ docker container prune --force || true
 
 echo "running postgres"
 docker run --detach --rm --network $FROBOTS_NETWORK --network-alias postgres-server -e POSTGRES_PASSWORD=$POSTGRES_PASS -e POSTGRES_USER=$POSTGRES_USER -v postgres_home:/home/${POSTGRES_USER} -v postgres_data:/var/lib/postgresql/data --name postgres postgres:12-bullseye
-echo "running the backend"
+echo "running the backend on: "$DATABASE_URL_NEW${dbname}
 docker run --rm -dp $PORT:$PORT -e GHOST_API_KEY -e SENDGRID_API_KEY -e SENDGRID_API_EXPORT_MAILINGLIST_KEY -e POOL_SIZE -e PORT -e DATABASE_URL=$DATABASE_URL_NEW${dbname} -e SECRET_KEY_BASE -e ADMIN_USER -e ADMIN_PASS --network $FROBOTS_NETWORK --network-alias frobots_backend -v web_certs:/var/certs --name frobots_backend elixir/frobots_backend
 #echo "running migrations"
 #docker exec frobots_backend bin/frobots_backend eval "FrobotsWeb.Release.migrate"
