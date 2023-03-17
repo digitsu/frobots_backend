8332ede498c569a9076d2de2ea87dd9fa214f510
digitsu <digitsu@gmail.com>
2023-03-17 16:43:11 +0900

    added S3 Store URL

--- config/runtime.exs
+++ config/runtime.exs
@@ -81,27 +81,38 @@ if config_env() == :prod || config_env() == :staging do
          :ghost_blog_url,
          "https://ghost.fubars.tech/ghost/api/content/posts/?key=#{ghost_api_key}"
 
-  S3_store_key =
-    System.get_env("LINODE_BACKEND_STORE_KEY") ||
+  S3_store_secret_key =
+    System.get_env("LINODE_BACKEND_STORE_SECRET_KEY") ||
       raise """
-      environment variable LINODE_BACKEND_STORE_KEY is missing.
+      environment variable LINODE_BACKEND_STORE_SECRET_KEY is missing.
       Did you forget to source env vars?
       """
 
   config :frobots_web,
-         :S3_store_key,
-         S3_store_key
+         :S3_store_secret_key,
+         S3_store_secret_key
 
-  S3_store_api =
-    System.get_env("LINODE_BACKEND_STORE") ||
+  S3_store_access_key =
+    System.get_env("LINODE_BACKEND_STORE_ACCESS_KEY") ||
       raise """
-      environment variable LINODE_BACKEND_STORE_KEY is missing.
+      environment variable LINODE_BACKEND_STORE_ACCESS_KEY is missing.
       Did you forget to source env vars?
       """
 
   config :frobots_web,
-         :S3_store_api,
-         S3_store_api
+         :S3_store_access_key,
+         S3_store_access_key
+
+  S3_store_url =
+    System.get_env("LINODE_BACKEND_STORE_URL") ||
+      raise """
+      environment variable LINODE_BACKEND_STORE_URL is missing.
+      Did you forget to source env vars?
+      """
+
+  config :frobots_web,
+         :S3_store_url,
+         S3_store_url
 
   # ## Using releases
   #
