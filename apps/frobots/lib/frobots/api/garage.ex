52c9887cc8f7ae851d2e5d9b2ef62dfa0dec4f5a
digitsu <digitsu@gmail.com>
2023-03-25 12:04:04 +0900

    Created FE Api stubs

---
+++ apps/frobots/lib/frobots/api/garage.ex
@@ -0,0 +1,10 @@
+defmodule Frobots.Api.Garage do
+
+  def create_frobot(_user, _name, _prototype) do
+    # create a frobot, with that name, using that prototype bot (name)
+    # equip the frobot will all the default parts and xframe
+      #create all the needed parts instances
+    # save it into the db
+    # return a frobot_name and id as success
+  end
+end
