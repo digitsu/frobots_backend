29df3441c7d4bff6639f0cd704a886fb923fcb34
digitsu <digitsu@gmail.com>
2023-03-25 13:38:51 +0900

    formatting

--- apps/frobots/lib/frobots/api/arena.ex
+++ apps/frobots/lib/frobots/api/arena.ex
@@ -2,11 +2,8 @@ defmodule Frobots.Api.Arena do
   # this is the FE api for liveview pages on the Arena
 
   def create_match(_user) do
-
   end
 
   def join_match(_user, _match) do
-    
   end
-
 end
