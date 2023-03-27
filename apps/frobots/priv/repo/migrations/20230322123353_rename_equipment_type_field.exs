f21b875fe6116c31e585e2fd1a97f4c5fe37580d
digitsu <digitsu@gmail.com>
2023-03-27 15:31:09 +0900

    equipment test fix

--- apps/frobots/priv/repo/migrations/20230322123353_rename_equipment_type_field.exs
+++ apps/frobots/priv/repo/migrations/20230322123353_rename_equipment_type_field.exs
@@ -2,6 +2,7 @@ defmodule Frobots.Repo.Migrations.RenameEquipmentTypeField do
   use Ecto.Migration
 
   def change do
+    drop index(:xframes, [:xframe_type])
     rename table("cannons"), :cannon_type, to: :type
     rename table("missiles"), :missile_type, to: :type
     rename table("scanners"), :scanner_type, to: :type
