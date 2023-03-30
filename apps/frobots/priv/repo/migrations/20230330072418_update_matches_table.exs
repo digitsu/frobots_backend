62c9fa0b2404cc1c5c36f2a76314f3e38325a1a4
Sumit Pugalia <sumit_pugalia@yahoo.com>
2023-03-30 13:23:07 +0530

    migration script for old_match to new_match

---
+++ apps/frobots/priv/repo/migrations/20230330072418_update_matches_table.exs
@@ -0,0 +1,13 @@
+defmodule Frobots.Repo.Migrations.UpdateMatchesTable do
+  use Ecto.Migration
+
+  def up do
+    execute(
+      "update matches m SET user_id = (select id from users where name = 'god' and email = 'frobots'), title = 'Default Match', Description = 'Migrated Matches', match_time = inserted_at, timer = 3600, arena_id = 1, min_player_frobot = CAST (match_template ->> 'min_frobots' AS INTEGER), max_player_frobot = CAST (match_template ->> 'max_frobots' AS INTEGER);"
+    )
+  end
+
+  def down do
+    IO.inspect("Nothing to Rollback;")
+  end
+end
