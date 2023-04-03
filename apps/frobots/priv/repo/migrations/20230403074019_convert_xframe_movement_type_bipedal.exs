87090f624f360c6fe68ed9ec0186e43e7c9456b8
jerry <digitsu@gmail.com>
2023-04-03 16:58:22 +0900

    formatting fun

--- apps/frobots/priv/repo/migrations/20230403074019_convert_xframe_movement_type_bipedal.exs
+++ apps/frobots/priv/repo/migrations/20230403074019_convert_xframe_movement_type_bipedal.exs
@@ -7,6 +7,7 @@ defmodule Frobots.Repo.Migrations.ConvertXframeMovementTypeBipedal do
     execute "update xframes set type = 'Chassis_Mk2' where type = 'Tank_Mk2'"
     execute "update xframes set type = 'Chassis_Mk3' where type = 'Tank_Mk3'"
   end
+
   def down do
     IO.inspect("nothing to rollback")
   end
