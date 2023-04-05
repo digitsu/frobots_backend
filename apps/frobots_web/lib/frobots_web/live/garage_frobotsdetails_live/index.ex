a0179ece0e7cb229ecc969186cc4797668efcfba
jishnudataequinox <jishnu@dataequinox.com>
2023-04-05 14:50:01 +0530

    Feat : frobot battlelogs API integration

--- apps/frobots_web/lib/frobots_web/live/garage_frobotsdetails_live/index.ex
+++ apps/frobots_web/lib/frobots_web/live/garage_frobotsdetails_live/index.ex
@@ -17,6 +17,13 @@ defmodule FrobotsWeb.GarageFrobotsDetailsLive.Index do
 
     case Api.get_frobot_details(String.to_integer(frobot_id)) do
       {:ok, frobot_details} ->
+        %{
+          entries: battles,
+          page_number: page,
+          page_size: page_size,
+          total_entries: total_entries
+        } = Api.list_paginated_frobot_battlelog([frobot_id: frobot_id], page: 1, page_size: 5)
+
         {:ok,
          socket
          |> assign(:user, current_user)
@@ -25,7 +32,11 @@ defmodule FrobotsWeb.GarageFrobotsDetailsLive.Index do
            Events.get_current_user_ranking_details(current_user)
          )
          |> assign(:frobot, frobot_details)
-         |> assign(:user_frobots, Assets.list_user_frobots(current_user))}
+         |> assign(:user_frobots, Assets.list_user_frobots(current_user))
+         |> assign(:battles, battles)
+         |> assign(:page, page)
+         |> assign(:page_size, page_size)
+         |> assign(:total_entries, total_entries)}
 
       {:error, message} ->
         {:ok,
@@ -59,7 +70,68 @@ defmodule FrobotsWeb.GarageFrobotsDetailsLive.Index do
      push_event(socket, "react.return_frobot_details", %{
        "frobotDetails" => frobot,
        "currentUser" => currentUser,
-       "userFrobots" => user_frobots
+       "userFrobots" => user_frobots,
+       "battles" => socket.assigns.battles,
+       "total_entries" => socket.assigns.total_entries,
+       "page" => socket.assigns.page,
+       "page_size" => socket.assigns.page_size
+     })}
+  end
+
+  def handle_event("react.filter_frobot_battle_logs", params, socket) do
+    filter_params = Keyword.new()
+
+    filter_params =
+      if params["frobot_id"],
+        do: Keyword.put(filter_params, :frobot_id, params["frobot_id"]),
+        else: filter_params
+
+    filter_params =
+      if params["match_status"],
+        do: Keyword.put(filter_params, :match_status, params["match_status"]),
+        else: filter_params
+
+    page_config = Keyword.new()
+
+    page_config =
+      if params["page"],
+        do: Keyword.put(page_config, :page, params["page"]),
+        else: Keyword.put(page_config, :page, 1)
+
+    page_config =
+      if params["page_size"],
+        do: Keyword.put(page_config, :page_size, params["page_size"]),
+        else: Keyword.put(page_config, :page_size, 5)
+
+    %{
+      entries: battles,
+      page_number: page,
+      page_size: page_size,
+      total_entries: total_entries
+    } = Api.list_paginated_frobot_battlelog(filter_params, page_config)
+
+    frobot = socket.assigns.frobot
+    current_user = socket.assigns.user
+    user_frobots = extract_frobots(socket.assigns.user_frobots)
+
+    currentUser = %{
+      "id" => current_user.id,
+      "avatar" => current_user.avatar,
+      "email" => current_user.email,
+      "name" => current_user.name,
+      "sparks" => current_user.sparks
+    }
+
+    {:noreply,
+     push_event(socket, "react.return_frobot_details", %{
+       "frobotDetails" => frobot,
+       "currentUser" => currentUser,
+       "userFrobots" => user_frobots,
+       "battles" => battles,
+       "total_entries" => total_entries,
+       "page" => page,
+       "page_size" => page_size,
+       "match_status" => params["match_status"]
      })}
   end
 
