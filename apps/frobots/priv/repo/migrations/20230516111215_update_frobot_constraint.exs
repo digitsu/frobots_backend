9bdac2ec6e0aa33dd2ce8633d6cc9f8a03092037
Sumit Pugalia <sumit_pugalia@yahoo.com>
2023-05-17 16:43:44 +0530

    formatting

--- apps/frobots/priv/repo/migrations/20230516111215_update_frobot_constraint.exs
+++ apps/frobots/priv/repo/migrations/20230516111215_update_frobot_constraint.exs
@@ -3,6 +3,7 @@ defmodule Frobots.Repo.Migrations.UpdateFrobotConstraint do
 
   def up do
     drop index(:slots, :frobot_id, name: :unique_frobot_id_slot)
+
     create unique_index(:slots, :frobot_id,
              where:
                "frobot_id IS NOT NULL AND match_type = 'real' And status != 'closed' AND status != 'done' AND status != 'cancelled' AND slot_type != 'protobot'",
@@ -12,11 +13,11 @@ defmodule Frobots.Repo.Migrations.UpdateFrobotConstraint do
 
   def down do
     drop index(:slots, :frobot_id, name: :unique_frobot_id_slot)
+
     create unique_index(:slots, :frobot_id,
              where:
                "frobot_id IS NOT NULL AND match_type = 'real' And status != 'closed' AND status != 'done' AND status != 'cancelled' AND slot_type != 'protobot'",
              name: :unique_frobot_id_slot
            )
-
   end
 end
