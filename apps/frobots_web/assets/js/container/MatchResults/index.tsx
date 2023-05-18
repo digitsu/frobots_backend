71d4d48854ccc346fe90ca5d7e4ea6ae4953f2b4
hari-dataequinox <hari@dataequinox.com>
2023-05-18 13:03:34 +0530

    Feat : Change No Match results style

--- apps/frobots_web/assets/js/container/MatchResults/index.tsx
+++ apps/frobots_web/assets/js/container/MatchResults/index.tsx
@@ -5,26 +5,38 @@ export default (props) => {
   const { match_results, s3_base_Url } = props
   return (
     <Box width={'90%'} m={'auto'} mt={8}>
-      <Grid container spacing={2}>
-        {match_results?.map(
-          ({ frobot, health, kills, xp_earned, user_name, winner }) => (
-            <Grid item xs={12}>
-              <ResultItem
-                key={frobot.id}
-                frobot={frobot}
-                health={health}
-                kills={kills}
-                xp_earned={xp_earned}
-                user_name={user_name}
-                winner={winner}
-                s3_base_Url={s3_base_Url}
-              />
-            </Grid>
-          )
-        )}
-      </Grid>
-      {match_results.length === 0 && (
-        <Typography>No Results for this Match</Typography>
+      {match_results.length === 0 ? (
+        <Grid
+          container
+          justifyContent={'center'}
+          alignItems={'center'}
+          minHeight={'calc(100vh - 50%)'}
+        >
+          <Grid item>
+            <Box>
+              <Typography variant="h3">Match ended with no results.</Typography>
+            </Box>
+          </Grid>
+        </Grid>
+      ) : (
+        <Grid container spacing={2}>
+          {match_results?.map(
+            ({ frobot, health, kills, xp_earned, user_name, winner }) => (
+              <Grid item xs={12}>
+                <ResultItem
+                  key={frobot.id}
+                  frobot={frobot}
+                  health={health}
+                  kills={kills}
+                  xp_earned={xp_earned}
+                  user_name={user_name}
+                  winner={winner}
+                  s3_base_Url={s3_base_Url}
+                />
+              </Grid>
+            )
+          )}
+        </Grid>
       )}
     </Box>
   )
