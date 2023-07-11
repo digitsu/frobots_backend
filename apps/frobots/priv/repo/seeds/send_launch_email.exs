af56bce5173aef21ca1acbcd2c40a94592585d33
Sumit Pugalia <sumit_pugalia@yahoo.com>
2023-07-11 09:48:15 +0530

    send launch email script

--- apps/frobots/priv/repo/seeds/send_launch_email.exs
+++ apps/frobots/priv/repo/seeds/send_launch_email.exs
@@ -2,18 +2,18 @@ alias FrobotsWeb.{BetaMailer, MailTemplates}
 alias Frobots.{Accounts, Mailer}
 alias FrobotsWeb.SendgridApi
 
-base_url = "https://api.sendgrid.com" #Application.get_env(:sendgrid, :base_url)
+base_url = Application.get_env(:sendgrid, :base_url)
 {:ok, emails} = SendgridApi.get_contacts(base_url)
 
 for email <- emails do
   user = Accounts.get_user_by_email(email)
   {:ok, user} =
-  if is_nil(user) do
-    new_user = %{"email" => email, "password" => "Secret!@#", "name" => email}
-    Accounts.register_user(new_user)
-  else
-    {:ok, user}
-  end
+    if is_nil(user) do
+      new_user = %{"email" => email, "password" => "Secret!@#", "name" => email}
+      Accounts.register_user(new_user)
+    else
+      {:ok, user}
+    end
 
   template = MailTemplates.beta_launch_mail_template()
   BetaMailer.welcome(%{name: user.name, email: user.email}, template)
