5281486cc533713d8852cf8ae48ab157f88d6cbe
jishnu-dataequinox <jishnu@dataequinox.com>
2023-08-22 13:30:52 +0530

    Fix: blockCopyToStorage multiple initialization

--- apps/frobots_web/assets/js/container/Garage/BlocklyEditor.tsx
+++ apps/frobots_web/assets/js/container/Garage/BlocklyEditor.tsx
@@ -3,6 +3,11 @@ import { BlocklyWorkspace } from 'react-blockly'
 import { Box } from '@mui/material'
 import { darkTheme } from '../../theme/blocklyTheme'
 import { MY_TOOLBOX } from '../../utils/toolbarConfig'
+import { CrossTabCopyPaste } from '@blockly/plugin-cross-tab-copy-paste'
+
+// Initiate cross tab copy paste feature
+const plugin = new CrossTabCopyPaste()
+plugin.init({ contextMenu: true, shortcut: true })
 
 export const BlocklyEditor: React.FC<any> = ({
   defaultXml = '',
