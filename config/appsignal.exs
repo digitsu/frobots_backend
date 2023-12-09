1eef6b127bdddd95a5d950ee81a5c4d1a4e167c3
Sumit Pugalia <sumit_pugalia@yahoo.com>
2023-12-09 11:10:33 +0530

    appsigal monitoring

--- config/appsignal.exs
+++ config/appsignal.exs
@@ -3,5 +3,5 @@ import Config
 config :appsignal, :config,
   otp_app: nil,
   name: "frobots",
-  push_api_key: "2b005568-b63f-4337-9ef3-23f748d33173",
+  push_api_key: "84c5781c-a791-47cf-b2c7-ef1ac4ea1739",
   env: Mix.env()
